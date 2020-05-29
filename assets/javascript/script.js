e5390c81117534cfb651a905a58d778b93186f1d
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2020-05-28 21:54:08 -0400

    hoizontal scroll container structure implimented

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,43 +9,6 @@ $(document).ready(function () {
 
   $(".navbar-toggler").toggleClass('show');
 
-  // $("#weatherButton").click(function() {
-
-  //   window.open("https://nicholasmcconnell.github.io/homework-six/");
-  // });
-
-  // $("#weatherButtonRepo").click(function() {
-
-  //   window.open("https://github.com/nicholasmcconnell/homework-six");
-  // });
-
-  // $("#plannerButton").click(function() {
-
-  //   window.open("https://nicholasmcconnell.github.io/homework-five/");
-  // });
-
-  // $("#plannerButtonRepo").click(function() {
-
-  //   window.open("https://github.com/nicholasmcconnell/homework-five");
-  // });
-
-  // $("#zillowButton").click(function() {
-
-  //   window.open("https://nicholasmcconnell.github.io/project1_Group_Project/");
-  // });
-
-  // $("#zillowButton").click(function() {
-
-  //   window.open("https://github.com/nicholasmcconnell/project1_Group_Project");
-  // });
-
-  //   $('#bt1').click(function() {
-  //     $('#contact-form').attr('action',
-  //                    'mailto:nicholas.mcconnell@gmail.com?subject=' +
-  //                    $('#subject').val() + '&body=' + $('#message').val());
-  //     $('#contact-form').submit();
-  // });
-
   $(".fa-linkedin").click(function () {
 
     window.open("http://linkedin.com/in/nickmcconnellmusic/")
@@ -62,43 +25,5 @@ $(document).ready(function () {
     window.open("assets/images/Nick McConnell Coding Resume - 5_18_20.pdf")
 
   });
-
-  // $("#pdf").click(function(){
-
-  //   window.open(this.href);
-  // });
-
-  // $(".button").click(function() {
-
-  //   // let id = JSON.stringify(this.id);
-  //   // console.log(id);
-
-  //   if (this.id === "weatherButton"){
-
-  //       window.open("https://nicholasmcconnell.github.io/homework-six/");
-
-  //   } else if (this.id === "plannerButton"){
-
-  //       window.open("https://nicholasmcconnell.github.io/homework-five/");
-
-  //   } else if (this.id === "zillowButton") {
-
-  //       window.open("https://nicholasmcconnell.github.io/project1_Group_Project/");
-
-  //   } else if (this.id === "weatherButtonRepo"){
-
-  //     window.open("https://github.com/nicholasmcconnell/homework-six");
-
-  // } else if (this.id === "plannerButtonRepo"){
-
-  //     window.open("https://github.com/nicholasmcconnell/homework-five");
-
-  // } else if (this.id === "zillowButtonRepo") {
-
-  //     window.open("https://github.com/nicholasmcconnell/project1_Group_Project/");
-  // }
-
-  // });
-
 
 })
